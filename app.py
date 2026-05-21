a870c7a53da983ba4061416f412908c35da9453d
B Ammulu <145189145+8977115766@users.noreply.github.com>
2026-05-21 17:22:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -173,11 +173,7 @@ Phone: {phone}
         """
 
     except Exception as e:
-
-        return f"""
-        <h2>Email Error</h2>
-        <p>{e}</p>
-        """
+    return str(e)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
